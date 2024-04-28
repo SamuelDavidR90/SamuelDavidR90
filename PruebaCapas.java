e6dcec372c280769ffcbb72ee3367b206f7c4071
SamuelDavidR90 <168297366+SamuelDavidR90@users.noreply.github.com>
2024-04-27 19:07:16 -0500

    Add files via upload
    Un acercamiento a los patrones de diseños, específicamente al MVC (Modelo Vista Controlador)
    En este modelo de capas encontramos la comunicación entre la IGU (Interfaz Grafica de Usuario) Usando para ello la Libreria Jframe, La capa Logica a traves de un sencillo ejemplo, quedando pendiente la parte de Persistencias.

---
+++ PruebaCapas.java
@@ -0,0 +1,15 @@
+
+
+package ejercicio.pruebacapas;
+
+import igu.Pantalla;
+
+
+public class PruebaCapas {
+
+    public static void main(String[] args) {
+        Pantalla panta = new Pantalla();
+        panta.setVisible(true);
+        panta.setLocationRelativeTo(null);
+    }
+}
